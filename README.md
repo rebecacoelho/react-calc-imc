cf562b33a718095014ea338f138a3cb3ea2dc9de
Rebeca Coelho <rebecasantoscoelho@gmail.com>
2022-09-20 14:01:49 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Calculador de IMC
+
+Projeto em React + Typescript fazendo uma calculadora de IMC.
+
+Projeto desenvolvido durante o curso [B7web](https://b7web.com.br)
+
+## Instalação
+- `npm install`
+
+### Pra rodar
+- `npm start`
---
+++ src/components/GridItem/GridItem.tsx
@@ -0,0 +1,36 @@
+import { Level } from "../../helpers/imc"
+import styles from './GridItem.module.css';
+import upImage from '../../assets/up.png';
+import downImage from '../../assets/down.png';
+
+
+type Props = {
+    item: Level
+}
+
+/*  OUTRA MANEIRA DE CONDICIONAL
+
+    {item.icon === 'up' && <img src={upImage} alt="" width="30"/>}
+    {item.icon === 'down' && <img src={downImage} alt="" width="30"/>}
+*/
+
+export const GridItem = ({ item }: Props) => {
+    return (
+        <div className={styles.main} style={{ backgroundColor: item.color }}>
+            <div className={styles.gridIcon}>
+                <img src={item.icon === 'up' ? upImage : downImage} alt="" width="30"/>
+            </div>
+            <div className={styles.gridTitle}>{item.title}</div>
+
+            {item.yourImc && 
+                <div className= {styles.yourImc}>Seu IMC é de {item.yourImc} kg/m²</div>
+            }
+
+            <div className={styles.gridInfo}>
+                <>
+                    IMC está entre <strong>{item.imc[0]}</strong> e <strong>{item.imc[1]}</strong>
+                </>
+            </div>
+        </div>
+    );
+}
